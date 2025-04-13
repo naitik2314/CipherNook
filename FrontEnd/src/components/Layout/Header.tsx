6b7c20b0b63aa78b7712eb0d8d725f5ada76fff4
Naitik Shah <naitik.shah@sakec.ac.in>
2025-04-12 20:00:56 -0400

    Removed duplicate Password buttons

--- FrontEnd/src/components/Layout/Header.tsx
+++ FrontEnd/src/components/Layout/Header.tsx
@@ -42,13 +42,6 @@ export const Header: React.FC<HeaderProps> = ({
         </div>
         {activeView === 'vault' && (
           <div>
-            <button
-              onClick={() => setIsModalOpen(true)} // Open the modal on button click
-              className="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-lg shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors duration-200"
-            >
-              <PlusIcon size={18} className="mr-1" />
-              <span>New</span>
-            </button>
             <AddPasswordModal
               isOpen={isModalOpen}
               onClose={() => setIsModalOpen(false)}
